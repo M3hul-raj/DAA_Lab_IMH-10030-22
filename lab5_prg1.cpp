dac7219b43ce56bfb137b62aafbdc5c4b2ea110a
M3hul-raj <143120371+M3hul-raj@users.noreply.github.com>
2024-10-23 09:41:05 +0530

    Update lab5_prg1.cpp

--- lab5_prg1.cpp
+++ lab5_prg1.cpp
@@ -1,3 +1,14 @@
+/* Element left after reductions. Given an array of positive elements. You need to perform a
+reduction operation. In each reduction operation the smallest positive element value is
+picked and all the elements are subtracted by that value. You need to print the number of
+elements left after each reduction process.
+Input: [5, 1, 1, 1, 2, 3, 5]
+Output:
+4 corresponds to [4, 1, 2, 4]
+3 corresponds to [3, 1, 3]
+2 corresponds to [2, 2]
+0 corresponds to [0]. */
+
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
